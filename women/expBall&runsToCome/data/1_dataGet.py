f7315790a38ca8ac4090412101fb39b93b8c7bd6
jodrna <jcullen638@gmail.com>
2026-04-09 11:10:40 +0100

    Fix gitignore: keep python in data/outputs, ignore other files

---
+++ women/expBall&runsToCome/data/1_dataGet.py
@@ -0,0 +1,137 @@
+import sqlalchemy
+import pandas as pd
+import numpy as np
+from urllib.parse import quote
+from datetime import datetime, timedelta
+
+
+engine = sqlalchemy.create_engine('postgresql://jordan:%s@77.68.112.208:5432/postgres' % quote('cricket123'))
+connection = engine.connect()
+
+# format_date_new = last_date.strftime("%m/%d/%Y")
+format_date = '12/31/2014'
+
+param = [(format_date,)] #here we convert the format_date to a tuple, because the method of inserting variables into the SQL query requires either a tuple or a dictionary. To make a dictionary we would need to put a placeholder name in the sql query but for some reason this doesn't work with postgres Sql (according to gpt)
+sql_query ='''select  matchid, id, tier, date, year, competition, venue, host, home, away, battingteam, innings, ball, delivery2, runs, score, t_runs, wicket, wickets, t_wickets,
+                                 ballsremaining, target, reduced, max_balls, noball, wide, ord, byes, legbyes, innperiod, bowlerwicket, realexprbat, realexpwbat, rating_sample_size, batsmanballs, batsman, nonstriker
+                                 from match_data.w_t20_bbb tb
+                                 where tier >= 0
+                                 and date > %s
+                                 and reduced is not true
+                                 order by matchid, innings, id'''
+
+sql_data = pd.read_sql_query(sql_query, con=connection, params=(format_date,)) #param is the format_date in this case. It is subbed in for %s within the query. You could have multiple %s in the query and multiple parameters in order
+
+
+# # export then import again, simple way to get date format correct
+sql_data.to_csv('/Users/jordan/Documents/ArmadaCricket/Development/women/data/sqldataforclean2023.csv', index=False)
+raw_data = pd.read_csv('/Users/jordan/Documents/ArmadaCricket/Development/women/data/sqldataforclean2023.csv')
+
+# use only tier 1 then some sorting
+raw_data = raw_data[raw_data['tier'] == 1]
+raw_data = raw_data.sort_values(by=['matchid', 'innings', 'delivery2'])
+uniques = raw_data['date'].apply(type).unique()
+
+# some cleaning, set target in 1st innings to nan, set reduced game to false when nan, change duplicate venue names
+raw_data['target'] = np.where(raw_data['innings'] == 1, np.nan, raw_data['target'])
+raw_data['reduced'] = raw_data['reduced'].fillna(False)
+raw_data = raw_data.dropna(subset=['battingteam'], axis=0)
+raw_data['venue'] = np.where(raw_data['venue'] == 'R.Premadasa Stadium', 'R Premadasa Stadium', raw_data['venue']) # could add to this (chinniswamy)
+
+# make sure targets are 1 more than 1st innings total
+targets = pd.pivot_table(raw_data[raw_data['innings'] == 1], values=['t_runs'], index=['matchid'], aggfunc=['mean']).reset_index()
+targets['innings'] = 2
+targets.columns = ['matchid', 'target_x', 'innings']
+targets['target_x'] = targets['target_x'] + 1
+raw_data = raw_data.merge(targets, how='left', on=['matchid', 'innings'])
+raw_data['target'] = raw_data['target_x']
+raw_data = raw_data.drop(labels=['target_x'], axis=1)
+
+
+# take out big bash after 2019 season because of the power surge
+raw_data = raw_data[(raw_data['competition'] != 'Women\'s Big Bash League') | (raw_data['date'] < '2020-06-06')]
+raw_data = raw_data[(raw_data['competition'] != 'Charlotte Edwards Cup') | (raw_data['date'] > '2022-01-01')]
+raw_data = raw_data[raw_data['competition'] != 'The Hundred Women\'s Competition']
+
+
+# this is the start of removing reduced games
+pivot = pd.pivot_table(raw_data, values=['t_runs', 't_wickets', 'max_balls', 'ball', 'noball', 'wide', 'target'], index=['matchid', 'innings', 'reduced'],
+                       aggfunc={'t_runs': 'max', 't_wickets': 'max', 'max_balls': 'min', 'ball': 'count', 'noball': 'sum', 'wide': 'sum', 'target': 'max'}).reset_index()
+
+# 1st innings reduced games
+pivot_1 = pivot.copy()
+pivot_1 = pivot_1[pivot_1['innings'] == 1]
+pivot_1['innings_balls'] = pivot_1['ball'] - pivot_1['wide'] - pivot_1['noball']
+# remove where max balls less than 120 but greater than 0 (because 0 max balls has errors, we'll deal with it separately)
+pivot_1['remove'] = np.where((pivot_1['innings'] == 1) & (pivot_1['max_balls'] < 120) & (pivot_1['max_balls'] > 0), 1, 0)
+# remove where max balls = 120, reduced = true and innings_balls < 120, sometimes max balls is 120 & only 2nd innings was reduced, here we can keep the first innings but only if there are 120 balls in the data
+pivot_1['remove'] = np.where((pivot_1['innings'] == 1) & (pivot_1['max_balls'] == 120) & (pivot_1['reduced'] == True) & (pivot_1['innings_balls'] < 115), 1, pivot_1['remove'])
+# where max balls is 0, look at innings_balls, if its 120 then not reduced. If it's less than 118 then remove unless there are 10 wickets
+pivot_1['remove'] = np.where((pivot_1['innings'] == 1) & (pivot_1['max_balls'] == 0) & (pivot_1['innings_balls'] > 117), 0, pivot_1['remove'])
+pivot_1['remove'] = np.where((pivot_1['innings'] == 1) & (pivot_1['max_balls'] == 0) & (pivot_1['innings_balls'] < 118) & (pivot_1['t_wickets'] < 10), 1, pivot_1['remove'])
+
+# 2nd innings reduced games
+pivot_2 = pivot.copy()
+pivot_2 = pivot_2[pivot_2['innings'] == 2]
+pivot_2['innings_balls'] = pivot_2['ball'] - pivot_2['wide'] - pivot_2['noball']
+# if sql says reduced mark as reduced
+pivot_2['remove'] = np.where((pivot_2['innings'] == 2) & (pivot_2['reduced'] == True), 1, 0)
+# remove where max balls less than 120 but greater than 0 (because 0 max balls has errors, we'll deal with it separately)
+pivot_2['remove'] = np.where((pivot_2['innings'] == 2) & (pivot_2['max_balls'] < 120) & (pivot_2['max_balls'] > 0), 1, pivot_2['remove'])
+# look at non-reduced games with 120 max balls, check first if 120 bowled, if not then look if target reached or bowled out, mark as reduced accordingly
+pivot_2['remove'] = np.where((pivot_2['innings'] == 2) & (pivot_2['reduced'] == False) & (pivot_2['max_balls'] == 120) & (pivot_2['innings_balls'] < 114) & (pivot_2['t_runs'] < pivot_2['target']) &
+                             (pivot_2['t_wickets'] < 10), 1, pivot_2['remove'])
+pivot_2['remove'] = np.where((pivot_2['innings'] == 2) & (pivot_2['reduced'] == False) & (pivot_2['max_balls'] == 0) & (pivot_2['innings_balls'] < 114) & (pivot_2['t_runs'] < pivot_2['target']) &
+                             (pivot_2['t_wickets'] < 10), 1, pivot_2['remove'])
+
+# merge the reduced into the raw data then remove reduced games
+pivot = pd.concat([pivot_1, pivot_2], axis=0)
+raw_data = raw_data.merge(pivot.loc[: , ['matchid', 'innings', 'remove']], how='left', on=['matchid', 'innings'])
+raw_data['remove'] = np.where(raw_data['competition'] == 'The Hundred Women\'s Competition', 0, raw_data['remove'])
+raw_data = raw_data[raw_data['remove'] == 0]
+raw_data = raw_data.drop(labels=['reduced', 'remove', 'max_balls'], axis=1) # same as dropping columns
+
+# fix the ball
+raw_data['extra'] = np.where(raw_data['wide'] + raw_data['noball'] > 0, 1, 0)
+raw_data['over_number'] =  raw_data['delivery2'].apply(lambda x: np.floor(x))
+rollextra = pd.DataFrame(raw_data.groupby(['matchid', 'over_number', 'innings'], sort=False)['extra'].rolling(50, min_periods=1, closed='left').sum()).reset_index().fillna(0)
+rollextra = rollextra.sort_values(by=['matchid', 'level_3']).reset_index(drop=True)
+rollextra['extra'] = rollextra['extra'] / 100
+# now assign
+raw_data = raw_data.reset_index(drop=True)
+raw_data['extra'] = rollextra['extra']
+raw_data['ball'] = raw_data['delivery2'] - raw_data['extra']
+raw_data['ballsremaining'] = np.where(raw_data['competition'] == 'The Hundred Women\'s Competition', round((100-((np.floor(raw_data['ball'])*5)+((raw_data['ball']-np.floor(raw_data['ball']))*100)-1)), 0),
+                                      round((120-((np.floor(raw_data['ball'])*6)+((raw_data['ball']-np.floor(raw_data['ball']))*100)-1)), 0))
+# use the hundred only after the powerplay
+raw_data = raw_data[(raw_data['ballsremaining'] <= 75) | (raw_data['competition'] != 'The Hundred Women\'s Competition')]
+
+
+# fix the score, first find games where total score != stated score, for these games stick with stated score, for others go to rolling score
+runs_comp = pd.pivot_table(raw_data, values=['runs', 't_runs'], index=['matchid', 'innings'], aggfunc={'runs': 'sum', 't_runs': 'mean'}).reset_index()
+runs_comp['comp'] = runs_comp['runs'] - runs_comp['t_runs']
+true_score = pd.DataFrame(raw_data.groupby(['matchid', 'innings'], sort=False)['runs'].rolling(200, min_periods=1, closed='left').sum()).reset_index().fillna(0)
+raw_data = raw_data.reset_index(drop=True)
+raw_data['true_score'] = true_score['runs'] # does this join them effectively?
+raw_data = raw_data.merge(runs_comp.loc[:, ['matchid', 'innings', 'comp']], on=['matchid', 'innings'], how='left')
+raw_data['score'] = np.where(raw_data['comp'] != 0, raw_data['score'], raw_data['true_score'])
+
+# clean
+raw_data['wickets'] = raw_data['wickets'] - raw_data['wicket']
+raw_data['wickets'] = np.where(raw_data['wickets'] == -1, 0, raw_data['wickets'])
+raw_data['required'] = raw_data['target'] - raw_data['score']
+raw_data['runs_to_come'] = raw_data['t_runs'] - raw_data['score']
+raw_data['result'] = np.where(raw_data['innings'] == 1, np.nan, np.where(raw_data['t_runs'] >= raw_data['target'], 1, 0))
+
+# final filter, remove obvious errors, wickets greater than 9, required must be more than 0 when innings is 2, ballsremaining must be >0, score must be >-1
+raw_data = raw_data[raw_data['wickets'] < 10]
+raw_data = raw_data[(raw_data['required'] > 0) | (raw_data['innings'] == 1)]
+raw_data = raw_data[raw_data['ballsremaining'] > 0]
+raw_data = raw_data[raw_data['score'] > -1]
+raw_data['over'] = raw_data['over_number'] + 1
+
+
+
+# export
+raw_data.to_csv('/Users/jordan/Documents/ArmadaCricket/Development/women/data/cleanedT20BBB.csv', index=False)
+
---
+++ women/expBall&runsToCome/data/2_dataClean.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+
+# import
+trainData = pd.read_csv('/Users/jordan/Documents/ArmadaCricket/Development/women/data/Cleaned_t20bbb3_adjusted_runs_to_come_w.csv', parse_dates=['date'])
+
+
+trainData = trainData.rename(columns={'innings': 'inningNumber', 'wickets': 'totalInningWickets', 'bowlerwicket': 'isWicketBowler', 'noball': 'noballRuns', 'over': 'overNumber',
+                                      'score': 'totalInningRuns', 't_runs': 'totalInningRunsEnd', 'runs_to_come': 'totalInningRunsToCome', 'wicket': 'isWicket',
+                                      't_wickets': 'totalInningWicketsEnd', 'runs': 'totalRuns', 'wide': 'wideRuns', 'ballsremaining': 'inningBallsRemaining', 'byes': 'byeRuns'})
+
+
+# difference in days between the ball and the first ball in the db
+trainData['year'] = trainData['date'].dt.year
+trainData['daysGroup'] = (trainData['date'] - (trainData['date'].min())).dt.days / 365
+
+# create some columns
+trainData['overBallNumber'] = (trainData['ball'] + 1 - trainData['overNumber']) * 100
+trainData['inningBallNumber'] = 121 - trainData['inningBallsRemaining']
+trainData['isPowerplay'] = np.where(trainData['inningBallNumber'] <= 36, 1, 0)
+trainData['isValid'] = np.where((trainData['wideRuns'] > 0) | (trainData['noballRuns'] > 0), 0, 1)
+trainData['isWide'] = np.where(trainData['wideRuns'] > 0, 1, 0)
+trainData['isNoball'] = np.where(trainData['noballRuns'] > 0, 1, 0)
+trainData['sample'] = 1
+trainData['totalInningWicketsToCome'] = trainData['totalInningWicketsEnd'] - trainData['totalInningWickets']
+trainData['batsmanRuns'] = trainData['totalRuns'] - trainData['noballRuns'] - trainData['wideRuns'] - trainData['byeRuns']
+trainData['isWicketRunOut'] = np.where(trainData['isWicket'] > trainData['isWicketBowler'], 1, 0)
+trainData['chaseWin'] = np.where(trainData['totalInningRunsEnd'] >= trainData['target'], 1, 0)
+trainData['runsRequired'] = trainData['target'] - trainData['totalInningRuns']
+
+# take out big bash power surge and 10 wickets down, keep only matches after 1st Jan 2015
+trainData = trainData[(trainData['competition'] != 'Big Bash League') | (trainData['date'] < '06-06-2020')]
+# trainData['totalInningWickets'] = np.where(trainData['isWicket'] == True, trainData['totalInningWickets'] - 1, trainData['totalInningWickets'])
+trainData = trainData[trainData['totalInningWickets'] <= 9]
+trainData = trainData[trainData['totalInningWickets'] >= 0]
+trainData = trainData[trainData['date'] >= '01-01-2015']
+
+
+trainData = trainData.loc[:, ['matchid', 'id', 'tier', 'date', 'year', 'competition', 'venue', 'host', 'home', 'away', 'battingteam', 'inningNumber', 'totalRuns', 'totalInningRuns',
+                              'totalInningRunsEnd', 'isWicket', 'totalInningWickets', 'totalInningWicketsEnd', 'inningBallsRemaining', 'target', 'noballRuns', 'wideRuns', 'ord', 'byeRuns', 'legbyes',
+                              'innperiod', 'isWicketBowler', 'realexprbat', 'realexpwbat', 'rating_sample_size', 'major_nation', 'batsmanballs', 'ovrexpr', 'ovrexpw', 'batsman', 'nonstriker', 'extra',
+                              'true_score', 'comp', 'required', 'totalInningRunsToCome', 'result', 'overNumber', 'daysGroup', 'overBallNumber', 'inningBallNumber', 'isPowerplay', 'isValid',
+                              'isWide', 'isNoball', 'sample', 'totalInningWicketsToCome', 'batsmanRuns', 'isWicketRunOut', 'chaseWin', 'runsRequired', 'RA_sum']]
+
+
+trainData.columns = ['matchID', 'ID', 'tier', 'date', 'year', 'competition', 'venue', 'host', 'home', 'away', 'battingTeam', 'inningNumber',
+                     'totalRuns', 'totalInningRuns', 'totalInningRunsEnd', 'isWicket', 'totalInningWickets', 'totalInningWicketsEnd',
+                     'inningBallsRemaining', 'target', 'noballRuns', 'wideRuns',
+                     'ord', 'byeRuns', 'legbyeRuns', 'inningPhase', 'isWicketBowler', 'realexprbat', 'realexpwbat', 'rating_sample_size',
+                     'major_nation', 'batsmanBallsFaced', 'ovrexpr', 'ovrexpw',
+                     'batsmanName', 'nonstrikerName', 'extra', 'true_score', 'comp', 'required', 'totalInningRunsToCome', 'result',
+                     'overNumber', 'daysGroup', 'overBallNumber', 'inningBallNumber', 'isPowerplay', 'isValid', 'isWide', 'isNoball', 'sample',
+                     'totalInningWicketsToCome', 'batsmanRuns', 'isWicketRunOut', 'chaseWin', 'runsRequired', 'RA_Sum']
+
+# adjusted runs, used for the match market and year adjustment
+trainData['runsRequiredAdj'] = trainData['runsRequired'] - trainData['RA_Sum']
+trainData['totalInningRunsToComeAdj'] = trainData['totalInningRunsToCome'] - trainData['RA_Sum']
+
+# export the cleaned data
+trainData.to_csv('/Users/jordan/Documents/ArmadaCricket/Development/women/data/dataClean.csv', index=False)
+
