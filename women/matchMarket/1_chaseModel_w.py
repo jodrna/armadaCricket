c12519266af7a2d919a00f234a964fa2437c1cdc
WillHowie <will.howie@livecricketdata.com>
2026-05-19 15:04:30 +0100

    inserting lookup column for inserting into RAS

--- women/matchMarket/1_chaseModel_w.py
+++ women/matchMarket/1_chaseModel_w.py
@@ -176,7 +176,9 @@ chaseLookup['m_diff'] = chaseLookup['m_chaseWin%'] - chaseLookup['m_chaseWin%Liv
 years = pd.pivot_table(trainData, index=['totalInningWickets', 'runsRequired', 'inningBallsRemaining'], values=['m_chaseWin%'], aggfunc='mean').reset_index()
 chaseLookup = chaseLookup.merge(years, how='left', on=['totalInningWickets', 'runsRequired', 'inningBallsRemaining'], suffixes=('', 'Year'))
 
-
+#insert lookup column for inserting into RAS
+col_position = chaseLookup.columns.get_loc('m_chaseWin%')  # gets index of 'B'
+chaseLookup.insert(col_position, 'lookup', (chaseLookup['totalInningWickets'] + (chaseLookup['runsRequired'] / 1000) + (chaseLookup['inningBallsRemaining'] / 1000000)).round(6))
 
 # # use this to get the target win% in distribs, 51.30%
 # yearswins = pd.pivot_table(trainData, index=['totalInningWickets', 'inningBallsRemaining'], values=['chaseWin'], aggfunc='mean').reset_index()
