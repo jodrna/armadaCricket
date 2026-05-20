ad7055062925e54e41bdeaa44e68ec4c4a795926
WillHowie <will.howie@livecricketdata.com>
2026-05-20 12:18:36 +0100

    adding column for putting in RAS

--- women/expBall&runsToCome/6_runsToComeSTDYearModel_w.py
+++ women/expBall&runsToCome/6_runsToComeSTDYearModel_w.py
@@ -97,4 +97,8 @@ plt.show()
 
 masterLookup.to_csv(PROJECT_ROOT / 'women/expBall&runsToCome/outputs/6_masterLookup_w.csv', index=False)
 
-
+max_days_group = masterLookup['daysGroup'].max()
+ras_input = masterLookup[masterLookup.daysGroup == max_days_group]
+ras_input['code'] = ras_input['totalInningWickets'] + ((121 - ras_input['inningsBallNumber']) / 1000)
+ras_input = masterLookup.loc[:, ['code', 'sample', 'totalInningRunsToComeSimBiasSplineYear', 'totalInningRunsToComeSimSTDYear', 'totalInningsRunsToComeSimMin', 'totalInningsRunsToComeSimMax', 'totalInningsRunsToComeSimSkew', 'totalInningsRunsToComeSimKurt']]
+masterLookup.to_csv(PROJECT_ROOT / 'women/expBall&runsToCome/outputs/ras_input_innings.csv', index=False)
